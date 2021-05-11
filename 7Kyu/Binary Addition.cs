10c64a9c3c53f2ab293fcfcc01cfaa722cfc78e6
Youcef <mekki.d.youcef@outlook.fr>
2021-05-10 23:35:23 -0400

    7Kyu Binary Addition

---
+++ 7Kyu/Binary Addition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string AddBinary(int a, int b)
+    {
+        int c = a + b;
+        string binary = Convert.ToString(c, 2);
+
+        return binary;
+
+    }
+}
